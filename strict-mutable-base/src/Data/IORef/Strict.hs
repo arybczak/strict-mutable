036c37b7761790204da7bb8c0c59b929d859ff7c
Andrzej Rybczak <andrzej@rybczak.net>
2024-08-30 21:18:56 +0200

    Remove IORef conversions
    Similar reason to removal of MVar conversions.

--- strict-mutable-base/src/Data/IORef/Strict.hs
+++ strict-mutable-base/src/Data/IORef/Strict.hs
@@ -2,11 +2,6 @@
 module Data.IORef.Strict
   ( IORef'
 
-    -- * Conversions
-  , fromIORef'
-  , toIORef'
-  , atomicToIORef'
-
     -- * Operations
   , newIORef'
   , readIORef'
@@ -29,30 +24,6 @@ import qualified Data.IORef as Base
 -- | A strict (WHNF) variant of 'IORef'.
 newtype IORef' a = IORef' (Base.IORef a)
   deriving (Eq, NFData, NFData1)
-
--- | Convert a strict 'IORef'' to an 'IORef'.
-fromIORef' :: IORef' a -> IO (IORef a)
-fromIORef' (IORef' var) = pure var
-
--- | Convert an 'IORef' to a strict 'IORef''.
---
--- /Warning:/ it's up to you to ensure that no thunks are put inside the
--- 'IORef'' via operations on the source.
-toIORef' :: IORef a -> IO (IORef' a)
-toIORef' var = do
-  let var' = IORef' var
-  modifyIORef' var' id
-  pure var'
-
--- | Convert an 'IORef' to an 'IORef'' atomically.
---
--- /Warning:/ it's up to you to ensure that no thunks are put inside the
--- 'IORef'' via operations on the source 'IORef'.
-atomicToIORef' :: IORef a -> IO (IORef' a)
-atomicToIORef' var = do
-  let var' = IORef' var
-  atomicModifyIORef' var' (\a -> (a, ()))
-  pure var'
 
 -- | 'Base.newIORef' for 'IORef''.
 newIORef' :: a -> IO (IORef' a)
