a60db945dcc3283da2c5d45dadf423fadc2ff745
Andrzej Rybczak <andrzej@rybczak.net>
2026-06-16 21:44:48 +0200

    Drop ticks in names of all operations and types
    To make the modules forward-compatible with versions to-be-included in `base`.
    
    See https://github.com/haskell/core-libraries-committee/issues/341 for more
    information.

--- strict-mutable-base/src/Control/Concurrent/Chan/Strict.hs
+++ strict-mutable-base/src/Control/Concurrent/Chan/Strict.hs
@@ -1,47 +1,47 @@
 -- | For full documentation please refer to "Control.Concurrent.Chan".
 module Control.Concurrent.Chan.Strict
-  ( Chan'
+  ( Chan
 
     -- * Operations
-  , newChan'
-  , writeChan'
-  , readChan'
-  , dupChan'
-  , getChan'Contents
-  , writeList2Chan'
+  , newChan
+  , writeChan
+  , readChan
+  , dupChan
+  , getChanContents
+  , writeList2Chan
   ) where
 
 import Control.Exception (evaluate)
 import qualified Control.Concurrent.Chan as Base
 
 -- | A strict (WHNF) variant of 'Base.Chan'.
-newtype Chan' a = Chan' (Base.Chan a)
+newtype Chan a = Chan (Base.Chan a)
   deriving Eq
 
--- | 'Base.newChan' for 'Chan''.
-newChan' :: IO (Chan' a)
-newChan' = Chan' <$> Base.newChan
+-- | 'Base.newChan' for 'Chan'.
+newChan :: IO (Chan a)
+newChan = Chan <$> Base.newChan
 
--- | 'Base.writeChan' for 'Chan''.
+-- | 'Base.writeChan' for 'Chan'.
 --
 -- Evaluates the value to WHNF.
-writeChan' :: Chan' a -> a -> IO ()
-writeChan' (Chan' chan) a = Base.writeChan chan =<< evaluate a
+writeChan :: Chan a -> a -> IO ()
+writeChan (Chan chan) a = Base.writeChan chan =<< evaluate a
 
--- | 'Base.readChan' for 'Chan''.
-readChan' :: Chan' a -> IO a
-readChan' (Chan' chan) = Base.readChan chan
+-- | 'Base.readChan' for 'Chan'.
+readChan :: Chan a -> IO a
+readChan (Chan chan) = Base.readChan chan
 
--- | 'Base.dupChan' for 'Chan''.
-dupChan' :: Chan' a -> IO (Chan' a)
-dupChan' (Chan' chan) = Chan' <$> Base.dupChan chan
+-- | 'Base.dupChan' for 'Chan'.
+dupChan :: Chan a -> IO (Chan a)
+dupChan (Chan chan) = Chan <$> Base.dupChan chan
 
--- | 'Base.getChanContents' for 'Chan''.
-getChan'Contents :: Chan' a -> IO [a]
-getChan'Contents (Chan' chan) = Base.getChanContents chan
+-- | 'Base.getChanContents' for 'Chan'.
+getChanContents :: Chan a -> IO [a]
+getChanContents (Chan chan) = Base.getChanContents chan
 
--- | 'Base.writeList2Chan' for 'Chan''.
+-- | 'Base.writeList2Chan' for 'Chan'.
 --
 -- Evaluates the values to WHNF.
-writeList2Chan' :: Chan' a -> [a] -> IO ()
-writeList2Chan' = mapM_ . writeChan'
+writeList2Chan :: Chan a -> [a] -> IO ()
+writeList2Chan = mapM_ . writeChan
--- strict-mutable-base/src/Data/IORef/Strict.hs
+++ strict-mutable-base/src/Data/IORef/Strict.hs
@@ -1,64 +1,64 @@
 -- | For full documentation please refer to "Data.IORef".
 module Data.IORef.Strict
-  ( IORef'
+  ( IORef
 
     -- * Operations
-  , newIORef'
-  , readIORef'
-  , writeIORef'
-  , modifyIORef'
-  , atomicModifyIORef'
-  , atomicWriteIORef'
-  , mkWeakIORef'
+  , newIORef
+  , readIORef
+  , writeIORef
+  , modifyIORef
+  , atomicModifyIORef
+  , atomicWriteIORef
+  , mkWeakIORef
   ) where
 
 import Control.DeepSeq
 import Control.Exception (evaluate)
 import GHC.Exts (mkWeak#)
 import GHC.IO (IO(..))
-import GHC.IORef (IORef(..))
 import GHC.STRef (STRef(..))
 import GHC.Weak (Weak(..))
 import qualified Data.IORef as Base
+import qualified GHC.IORef as GHC
 
--- | A strict (WHNF) variant of 'IORef'.
-newtype IORef' a = IORef' (Base.IORef a)
+-- | A strict (WHNF) variant of 'Base.IORef'.
+newtype IORef a = IORef (Base.IORef a)
   deriving (Eq, NFData, NFData1)
 
--- | 'Base.newIORef' for 'IORef''.
+-- | 'Base.newIORef' for 'IORef'.
 --
 -- Evaluates the initial value to WHNF.
-newIORef' :: a -> IO (IORef' a)
-newIORef' a = fmap IORef' . Base.newIORef =<< evaluate a
+newIORef :: a -> IO (IORef a)
+newIORef a = fmap IORef . Base.newIORef =<< evaluate a
 
--- | 'Base.readIORef' for 'IORef''.
-readIORef' :: IORef' a -> IO a
-readIORef' (IORef' var) = Base.readIORef var
+-- | 'Base.readIORef' for 'IORef'.
+readIORef :: IORef a -> IO a
+readIORef (IORef var) = Base.readIORef var
 
--- | 'Base.writeIORef' for 'IORef''.
+-- | 'Base.writeIORef' for 'IORef'.
 --
 -- Evaluates the new value to WHNF.
-writeIORef' :: IORef' a -> a -> IO ()
-writeIORef' (IORef' var) a = Base.writeIORef var =<< evaluate a
+writeIORef :: IORef a -> a -> IO ()
+writeIORef (IORef var) a = Base.writeIORef var =<< evaluate a
 
--- | 'Base.modifyIORef' for 'IORef''.
-modifyIORef' :: IORef' a -> (a -> a) -> IO ()
-modifyIORef' (IORef' var) f = Base.modifyIORef' var f
+-- | 'Base.modifyIORef' for 'IORef'.
+modifyIORef :: IORef a -> (a -> a) -> IO ()
+modifyIORef (IORef var) f = Base.modifyIORef' var f
 
--- | 'Base.atomicModifyIORef' for 'IORef''.
+-- | 'Base.atomicModifyIORef' for 'IORef'.
 --
 -- Evaluates the new value to WHNF.
-atomicModifyIORef' :: IORef' a -> (a -> (a, b)) -> IO b
-atomicModifyIORef' (IORef' var) f = Base.atomicModifyIORef' var f
+atomicModifyIORef :: IORef a -> (a -> (a, b)) -> IO b
+atomicModifyIORef (IORef var) f = Base.atomicModifyIORef' var f
 
--- | 'Base.atomicWriteIORef' for 'IORef''.
+-- | 'Base.atomicWriteIORef' for 'IORef'.
 --
 -- Evaluates the new value to WHNF.
-atomicWriteIORef' :: IORef' a -> a -> IO ()
-atomicWriteIORef' (IORef' var) a = Base.atomicWriteIORef var =<< evaluate a
+atomicWriteIORef :: IORef a -> a -> IO ()
+atomicWriteIORef (IORef var) a = Base.atomicWriteIORef var =<< evaluate a
 
--- | 'Base.mkWeakIORef' for 'IORef''.
-mkWeakIORef' :: IORef' a -> IO () -> IO (Weak (IORef' a))
-mkWeakIORef' var@(IORef' (IORef (STRef var#))) (IO finalizer) = IO $ \s0 ->
+-- | 'Base.mkWeakIORef' for 'IORef'.
+mkWeakIORef :: IORef a -> IO () -> IO (Weak (IORef a))
+mkWeakIORef var@(IORef (GHC.IORef (STRef var#))) (IO finalizer) = IO $ \s0 ->
   case mkWeak# var# var finalizer s0 of
     (# s1, w #) -> (# s1, Weak w #)
